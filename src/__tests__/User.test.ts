07d62a7308d84c0614b698a6bd6168e7905ff9ad
Victoradireto <victor.f@adireto.com>
2021-02-24 09:46:59 -0300

    test:create integration test for surveys

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,28 +1,30 @@
 import request from 'supertest';
+
 import { app } from '../app';
 
 import createConnection from '../database';
 
-describe('Users', () =>{
+describe('Users', () => {
   beforeAll(async () => {
     const connection = await createConnection();
     await connection.runMigrations();
   });
 
-  it('Should be albe to create a new user', async () => {
+  it('Should be able to create a new user', async () => {
     const response = await request(app).post('/users').send({
       email: 'user@example.com',
-      name: 'User Example'
-    })
+      name: 'User Example',
+    });
+
     expect(response.status).toBe(201);
-  })
+  });
 
-  it('Should not  be albe to create a user with exists email', async () => {
+  it('should not be able to create a user with exist email', async () => {
     const response = await request(app).post('/users').send({
       email: 'user@example.com',
-      name: 'User Example'
-    })
-    expect(response.status).toBe(400);
-  })
-})
+      name: 'User Example',
+    });
 
+    expect(response.status).toBe(400);
+  });
+});
