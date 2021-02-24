4c94f70b1d612031355c8ca5065ee8f04a5a74f1
Victoradireto <victor.f@adireto.com>
2021-02-24 08:08:27 -0300

    feat: create UsersRepository

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,21 +1,24 @@
-import { Request, Response} from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
+
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email } = request.body;
     
-    const usersRepository = getRepository(User);
+    const usersRepository = getCustomRepository(UsersRepository);
 
     const userAlreadyExists = await usersRepository.findOne({
       email
     })
+    
     if(userAlreadyExists){
       return response.status(400).json({
         error: 'User already exists!',
       })
     }
+
     const user = usersRepository.create({
       name, email
     });
@@ -26,4 +29,4 @@ class UserController {
   }
 }
 
-export { UserController}
+export { UserController };
