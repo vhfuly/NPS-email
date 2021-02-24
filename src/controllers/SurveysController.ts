be3fb97194fa28737209f4f8d6d460a37f2441c4
Victoradireto <victor.f@adireto.com>
2021-02-24 08:31:54 -0300

    feat: add show surveys

--- src/controllers/SurveysController.ts
+++ src/controllers/SurveysController.ts
@@ -19,6 +19,14 @@ class SurveysController {
 
     return response.status(201).json(survey);
   }
+
+  async show(request: Request, response: Response) {
+    const surveysRepository = getCustomRepository(SurveysRepository);
+
+    const all = await surveysRepository.find();
+
+    return response.status(200).json(all);
+  }
 }
 
 export { SurveysController }
--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,7 @@ const surveysController = new SurveysController();
 
 router.post('/users', userController.create)
 router.post('/surveys', surveysController.create)
+router.get('/surveys', surveysController.show)
 
 export { router }
 
