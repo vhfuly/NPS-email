f168bc4697855507356ac639cbd19b05a2b6d0ae
Victoradireto <victor.f@adireto.com>
2021-02-25 08:14:23 -0300

    feat: create migration CreateSurveysUsers

---
+++ src/database/migrations/1614251080472-CreateSurveysUsers.ts
@@ -0,0 +1,42 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSurveysUsers1614251080472 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+              name: 'surveys_users',
+              columns: [
+                {
+                  name: 'id',
+                  type: 'uuid',
+                  isPrimary: true,
+                },
+                {
+                  name: 'user_id',
+                  type: 'uuid',
+                },
+                {
+                  name: 'survey_id',
+                  type: 'uuid',
+                },
+                {
+                  name: 'value',
+                  type: 'number',
+                  isNullable: true,
+                },
+                {
+                  name: 'created_at',
+                  type: 'timestamp',
+                  default: 'now()',
+                },
+              ],
+            }),
+          );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('surveys_users');
+    }
+
+}
