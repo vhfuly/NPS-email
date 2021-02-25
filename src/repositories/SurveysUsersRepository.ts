92144cafab427922b2db6340ae16b91d3d535697
Victoradireto <victor.f@adireto.com>
2021-02-25 08:39:53 -0300

    feat: create router sendMail

--- src/repositories/SurveysUsersRepository.ts
+++ src/repositories/SurveysUsersRepository.ts
@@ -1,6 +1,7 @@
-import { Repository } from 'typeorm';
+import { Repository, EntityRepository } from 'typeorm';
 import { SurveyUser } from '../models/SurveyUser';
 
+@EntityRepository(SurveyUser)
 class SurveysUsersRepository extends Repository<SurveyUser> {}
 
 export { SurveysUsersRepository }
