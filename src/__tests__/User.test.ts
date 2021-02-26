081cf77aad3ee02487b107a7150fde66614a70f6
Victoradireto <victor.f@adireto.com>
2021-02-26 20:18:48 -0300

    test: adding  after

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -3,12 +3,20 @@ import request from 'supertest';
 import { app } from '../app';
 
 import createConnection from '../database';
+import { getConnection } from 'typeorm';
 
 describe('Users', () => {
   beforeAll(async () => {
     const connection = await createConnection();
     await connection.runMigrations();
   });
+
+  afterAll(async () => {
+    const connection = getConnection();
+    await connection.dropDatabase();
+    await connection.close();
+  })
+
 
   it('Should be able to create a new user', async () => {
     const response = await request(app).post('/users').send({
